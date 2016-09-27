22880bb844c3c00a5c45f8e9138c61d22cceb5fb
Joan Lopez <joanjan14@gmail.com>
2016-09-27 19:00:56 +0200

    problem14 solved! (a bit slow)

--- src/problem14/problem14.erl
+++ src/problem14/problem14.erl
@@ -2,36 +2,14 @@
 -author("joan").
 
 %% API
--export([collatz_sequence/2, collatz_sequence_for/1]).
+-export([collatz_sequence/2, collatz_sequence_for/1, index_of/2]).
 
 collatz_sequence(1, _) ->
   [1];
 collatz_sequence(Start, Steps) when (Start rem 2) =:= 0 ->
-  case Steps of
-    -1 ->
-      case erlang:get({'collatz', Start}) of
-        undefined ->
-          erlang:put({'collatz', Start}, collatz_sequence(Start div 2, Steps-1, [Start])),
-          erlang:get({'collatz', Start});
-        _ ->
-          erlang:get({'collatz', Start})
-      end;
-    _ ->
-      collatz_sequence(Start div 2, Steps-1, [Start])
-  end;
+  collatz_sequence(Start div 2, Steps-1, [Start]);
 collatz_sequence(Start, Steps) when (Start rem 2) =/= 0 ->
-  case Steps of
-    -1 ->
-      case erlang:get({'collatz', Start}) of
-        undefined ->
-          erlang:put({'collatz', Start}, collatz_sequence((Start*3)+1, Steps-1, [Start])),
-          erlang:get({'collatz', Start});
-        _ ->
-          erlang:get({'collatz', Start})
-      end;
-    _ ->
-      collatz_sequence(Start div 2, Steps-1, [Start])
-  end.
+  collatz_sequence((Start*3)+1, Steps-1, [Start]).
 collatz_sequence(_, Steps, Sequence) when Steps =:= 0 ->
   Sequence;
 collatz_sequence(1, _, Sequence) ->
@@ -43,6 +21,17 @@ collatz_sequence(X, Steps, Sequence) when (X rem 2) =/= 0 ->
 
 collatz_sequence_for(List) ->
   lists:map(fun(X) -> collatz_sequence(X, -1) end, List).
+
+
+
+%% Index of from StackOverflow.com %%
+index_of(Item, List) -> index_of(Item, List, 1).
+
+index_of(_, [], _)  -> not_found;
+index_of(Item, [Item|_], Index) -> Index;
+index_of(Item, [_|Tl], Index) -> index_of(Item, Tl, Index+1).
+
+
 
 %% Memoization example with process dict %%
 %%collatz_sequence_for_memoization(List) ->
--- src/problem14/problem14_tests.erl
+++ src/problem14/problem14_tests.erl
@@ -8,3 +8,14 @@ collatz_sequence_test() ->
   ?assertEqual([13, 40, 20, 10], problem14:collatz_sequence(13, 4)),
   ?assertEqual([13, 40, 20, 10, 5, 16, 8, 4, 2, 1], problem14:collatz_sequence(13, 10)),
   ?assertEqual([13, 40, 20, 10, 5, 16, 8, 4, 2, 1], problem14:collatz_sequence(13, 20)).
+
+index_of_test() ->
+  ?assertEqual(1, problem14:index_of(1, [1,3,2])),
+  ?assertEqual(2, problem14:index_of(3, [1,3,2])),
+  ?assertEqual(3, problem14:index_of(2, [1,3,2])).
+
+problem14_test() ->
+  Sequences = problem14:collatz_sequence_for(lists:seq(1,1000000)),
+  Lengths  = lists:map(fun(List) -> length(List) end, Sequences),
+  Maximum = lists:max(Lengths),
+  ?assertEqual(837799, problem14:index_of(Maximum, Lengths)).
