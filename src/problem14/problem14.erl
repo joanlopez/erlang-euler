b594868dcb0da18eafdb1dfc0a97f6807027d8f7
Joan Lopez <joanjan14@gmail.com>
2016-09-27 15:40:10 +0200

    Memoization for problem14 (slow version)

--- src/problem14/problem14.erl
+++ src/problem14/problem14.erl
@@ -7,9 +7,31 @@
 collatz_sequence(1, _) ->
   [1];
 collatz_sequence(Start, Steps) when (Start rem 2) =:= 0 ->
-  collatz_sequence(Start div 2, Steps-1, [Start]);
+  case Steps of
+    -1 ->
+      case erlang:get({'collatz', Start}) of
+        undefined ->
+          erlang:put({'collatz', Start}, collatz_sequence(Start div 2, Steps-1, [Start])),
+          erlang:get({'collatz', Start});
+        _ ->
+          erlang:get({'collatz', Start})
+      end;
+    _ ->
+      collatz_sequence(Start div 2, Steps-1, [Start])
+  end;
 collatz_sequence(Start, Steps) when (Start rem 2) =/= 0 ->
-  collatz_sequence((Start*3)+1, Steps-1, [Start]).
+  case Steps of
+    -1 ->
+      case erlang:get({'collatz', Start}) of
+        undefined ->
+          erlang:put({'collatz', Start}, collatz_sequence((Start*3)+1, Steps-1, [Start])),
+          erlang:get({'collatz', Start});
+        _ ->
+          erlang:get({'collatz', Start})
+      end;
+    _ ->
+      collatz_sequence(Start div 2, Steps-1, [Start])
+  end.
 collatz_sequence(_, Steps, Sequence) when Steps =:= 0 ->
   Sequence;
 collatz_sequence(1, _, Sequence) ->
@@ -21,3 +43,18 @@ collatz_sequence(X, Steps, Sequence) when (X rem 2) =/= 0 ->
 
 collatz_sequence_for(List) ->
   lists:map(fun(X) -> collatz_sequence(X, -1) end, List).
+
+%% Memoization example with process dict %%
+%%collatz_sequence_for_memoization(List) ->
+%%  lists:map(
+%%    fun(X) ->
+%%      case erlang:get({'collatz', X}) of
+%%        undefined ->
+%%          erlang:put({'collatz', X}, collatz_sequence(X, -1)),
+%%          erlang:get({'collatz', X});
+%%        _ ->
+%%          erlang:get({'collatz', X})
+%%      end
+%%    end,
+%%    List
+%%  ).
